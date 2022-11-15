0c64d9b76b77f9b09eb3c9a5ef847fded1bb580b
eka-wariah <ekawrh11@gmail.com>
2022-11-15 22:34:10 +0700

    first commit

---
+++ latihan.php
@@ -0,0 +1,22 @@
+<?php 
+//echo "asdfghjk";
+	/*$awikwok = "awikwok";
+	echo $awikwok;
+	$a = 5;
+	$b = 2;
+	
+	var_dump($a);
+	$gararetek = ["gilang", 1 , "tayo"];
+
+	foreach ($gararetek as $asd) {
+		echo $asd;
+	}
+	$gararetek = [["gilang", 1 , "tayo"], ["gilang", 1 , "tayo"], ["gilang", 1 , "tayo"]];
+
+	foreach ($gararetek as $asd) {
+		foreach ($asd as $zhg){
+			echo $zhg;
+		}
+	}*/
+
+?>
