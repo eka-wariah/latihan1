1e0f6629c0cdc2e28d67055b9f043b5200edc1fe
eka-wariah <ekawrh11@gmail.com>
2022-11-22 22:11:50 +0700

    database

---
+++ coba2.php
@@ -0,0 +1,10 @@
+<?php
+
+$koneksi = mysqli_connect("localhost","root","","latihan");
+//include 'coba1.php';
+// if ($koneksi == true) {
+// 	echo "koneksi berhasil";
+// }else{
+// 	echo "koneksi gagal";
+// }
+?>
